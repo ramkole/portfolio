2956fb89dbfc87d1ec41eb1e7a4df02fa397dd6f
ramkole <ram_kole@yahoo.com>
2023-12-26 17:45:56 +0530

    About and Project section addup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import AboutMe from "@/components/AboutMe";
 import HeaderHero from "@/components/HeaderHero";
+import Projects from "@/components/ProjectSection";
+import SectionDivider from "@/components/SectionDivider";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <HeaderHero />
+      <SectionDivider />
+      <AboutMe />
+      <Projects />
     </main>
   );
 }
---
+++ components/AboutMe.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./SectionHeading";
+import { motion } from "framer-motion";
+// import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  // const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      //      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        After graduating with a degree in{" "}
+        <span className="font-medium">CSE</span>, I decided to pursue my passion
+        for programming. I enrolled in a coding bootcamp and learned{" "}
+        <span className="font-medium">Front End web development</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core stack
+        is <span className="font-medium">React, Next.js and React Native</span>.
+        I am also familiar with TypeScript and Prisma. I am always looking to
+        learn new technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a software
+        developer.
+      </p>
+
+      <p>
+        <span className="italic">When I&lsquo;m not coding</span>, I enjoy
+        playing mobile video games, watching movies, and farming. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
+        <span className="font-medium">diet and workouts to imporve health</span>{" "}
+        as i am working from my home.
+      </p>
+    </motion.section>
+  );
+}
