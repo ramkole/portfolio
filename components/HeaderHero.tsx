2b7622900ae528f3bbb5da6cb7d499bbc4a7ed91
ramkole <ram_kole@yahoo.com>
2023-12-26 13:41:13 +0530

    added &lsquo

--- components/HeaderHero.tsx
+++ components/HeaderHero.tsx
@@ -53,7 +53,7 @@ const HeaderHero = () => {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ram.</span> I'm a{" "}
+        <span className="font-bold">Hello, I&lsquo;m Ram.</span> I&lsquo;m a{" "}
         <span className="font-bold">frontEnd developer</span> with{" "}
         <span className="font-bold">7+ years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
