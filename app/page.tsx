6391044cdd6fa8ed23125619231aeba4bfa85a77
ramkole <ram_kole@yahoo.com>
2023-12-28 07:57:14 +0530

    Added skills

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AboutMe from "@/components/AboutMe";
 import HeaderHero from "@/components/HeaderHero";
 import Projects from "@/components/ProjectSection";
 import SectionDivider from "@/components/SectionDivider";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <SectionDivider />
       <AboutMe />
       <Projects />
+      <Skills />
     </main>
   );
 }
